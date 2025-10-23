67a1ee56ce3d09b5ad1d38870d6215047b1cf3e7
loa212 <anobileloris@gmail.com>
2025-10-23 15:36:57 +0200

    feat: biome

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import homepage from './src/pages/index.html';
+import homepage from "./src/pages/index.html";
 
 const server = Bun.serve({
 	port: 3000,
 	routes: {
 		"/": homepage,
-	}
+	},
 });
 
 console.log(`Listening on ${server.url}`);
